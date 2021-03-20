c079451754ab3dc5d9eded3eacb02281673f9d5e
sangharsh <sangharsh@gmail.com>
2021-03-20 07:57:40 +0000

    antd

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import 'antd/dist/antd.css';
 import './App.css';
+import { DatePicker, message } from 'antd';
 import SayHello from "antd-easy-crop";
 function App() {
+    const [date, setDate] = useState();
+    const handleChange = (value: any) => {
+        message.info(`Selected Date: ${value ? value.format('YYYY-MM-DD') : 'None'}`);
+        setDate(value);
+    };
   return (
       <div className="App">
+          <div style={{ width: 400, margin: '100px auto' }}>
+              <DatePicker onChange={handleChange} />
+              <div style={{ marginTop: 16 }}>
+                  {/*Selected Date: {date ? date.format('YYYY-MM-DD') : 'None'}*/}
+              </div>
+          </div>
           <SayHello name="Prateek" />
       </div>
   );
