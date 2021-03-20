c7e4d87286a6490d3e40924e86eaf0788a797a23
sangharsh <sangharsh@gmail.com>
2021-03-20 07:40:34 +0000

    LIB

---
+++ example/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import SayHello from "antd-easy-crop";
+function App() {
+  return (
+      <div className="App">
+          <SayHello name="Prateek" />
+      </div>
+  );
+}
+
+export default App;
